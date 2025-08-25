617fed911080e375efc31f3895f1a195f466f6ec
JunPyoSeo <cooljp894@gmail.com>
2025-08-25 23:27:13 +0900

    [COMMENT]
    주석 업데이트

--- Assets/02_Scripts/ObjectPool&Factory/Factory/Interface/IProduct.cs
+++ Assets/02_Scripts/ObjectPool&Factory/Factory/Interface/IProduct.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
 
 /// <summary>
-/// 팩토리에서 생성되는 객체가 반드시 구현해야 하는 인터페이스입니다.
+/// 팩토리(Factory) 패턴에서 생성되는 객체가 반드시 구현해야 하는 인터페이스입니다.
+/// <para>객체의 이름(식별자)과 초기화(Init) 등 생성/초기화 관련 기능을 정의합니다.</para>
 /// </summary>
 public interface IProduct
 {
     /// <summary>
-    /// 객체의 이름(식별자)
+    /// 객체의 이름(식별자). 팩토리/게임 내에서 오브젝트를 구분하는 데 사용됩니다.
     /// </summary>
     public string Name { get; set; }
     /// <summary>
-    /// 객체 초기화 메서드
+    /// 객체의 상태를 초기화하는 메서드. 팩토리에서 생성 또는 풀에서 꺼낼 때 호출됩니다.
     /// </summary>
     public void Init();
 }
