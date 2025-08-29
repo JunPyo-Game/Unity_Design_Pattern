cfc6441d1304f50485c193c7a1ae6e75d4e6f8a0
JunPyoSeo <cooljp894@gmail.com>
2025-08-29 22:05:38 +0900

    [MOVE] 폴더 이동
    State 폴더
    -> UnityChanDemo 폴더로 이동

--- Assets/02_Scripts/UnityChanDemo/FollowCam.cs
+++ Assets/02_Scripts/UnityChanDemo/FollowCam.cs
@@ -5,7 +5,7 @@ public class FollowCam : MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private Vector3 offset = new(0, 3, -4);
 
-    private void Update()
+    private void LateUpdate()
     {
         transform.position = target.position + target.rotation * offset;
         transform.LookAt(target.position);
